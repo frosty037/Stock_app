8819c8fe1a8e26f4a1610c845bc51696ff942938
frosty037 <131701277+frosty037@users.noreply.github.com>
2026-03-17 23:04:19 +0300

    Update m1Broker.py

--- m1Broker.py
+++ m1Broker.py
@@ -1,26 +1,29 @@
 import yfinance as yf
 import streamlit as st
 
-sifre = st.text_input("Şifre:", type="password")
-
-if sifre != "samet123":
-    st.stop()
 st.title("Hisse Fiyat Sorgulama")
+
+
+lots = ["LOGO", "ASELS"]
+
+for lot in lots:
+    veri = yf.Ticker(f"{lot}.IS").history(period="1d")
+    if not veri.empty:
+        veri = yf.Ticker(f"{lot}.IS").history(period="1mo", interval="1m")
+        fiyat = round(veri["Close"].iloc[-1], 2)
+
+        st.line_chart(veri["Close"])
 
 stock_name = st.text_input("Hisse adı gir:").strip().upper()
 
 if stock_name:
     stock_name += ".IS"
     share = yf.Ticker(stock_name)
-
-    periyot = st.selectbox("Periyot seç:", ["1d", "5d", "1mo", "3mo", "1y"])
-
-    gecmis = share.history(period=periyot)
-
+    
+    gecmis = share.history(period="1d")
+    
     if gecmis.empty:
         st.error("Hisse bulunamadı!")
     else:
         price = gecmis["Close"].iloc[-1]
         st.success(f"{stock_name}: {round(price, 2)} TL")
-
-        st.line_chart(gecmis["Close"])
