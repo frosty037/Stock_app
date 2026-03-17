447c76db6b6d0313bf88abede07e71ebb3dca759
frosty037 <131701277+frosty037@users.noreply.github.com>
2026-03-17 22:31:26 +0300

    Update m1Broker.py

--- m1Broker.py
+++ m1Broker.py
@@ -1,12 +1,6 @@
 import yfinance as yf
 import streamlit as st
 
-sifre = st.text_input("Şifre:", type="password")
-
-if sifre != "samet123":
-    st.stop()
-
-
 st.title("Hisse Fiyat Sorgulama")
 
 stock_name = st.text_input("Hisse adı gir:").strip().upper()
@@ -14,10 +8,11 @@ stock_name = st.text_input("Hisse adı gir:").strip().upper()
 if stock_name:
     stock_name += ".IS"
     share = yf.Ticker(stock_name)
-    bilgi = share.info
-    price = bilgi.get("currentPrice")
-
-    if price is None:
+    
+    gecmis = share.history(period="1d")
+    
+    if gecmis.empty:
         st.error("Hisse bulunamadı!")
     else:
-        st.success(f"{stock_name}: {price} TL")
+        price = gecmis["Close"].iloc[-1]
+        st.success(f"{stock_name}: {round(price, 2)} TL")
